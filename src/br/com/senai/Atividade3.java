88023217b5d2b37239f797b701caf699b404596c
Larissa71 <larissakuffel@gmail.com>
2021-05-10 16:26:40 -0300

    Exemplo de códigos utilizando While

---
+++ src/br/com/senai/Atividade3.java
@@ -0,0 +1,33 @@
+package br.com.senai;
+
+import java.util.Scanner;
+
+public class Atividade3 {
+	public static void main(String[] args) {
+	
+		Scanner tec = new Scanner(System.in);
+		System.out.print("informe o primeiro valor: ");
+		int primeiroValor = tec.nextInt();
+		System.out.print("informe o segundo valor: ");
+		int segundoValor = tec.nextInt();
+
+		int par = 0;
+		int impar = 0;
+				
+		if(primeiroValor < segundoValor) {
+		while(primeiroValor <= segundoValor) {
+			}
+		}if(par % 2 == 0) {
+			par++1 
+		}
+		}else{
+		while(primeiroValor > segundoValor) {
+		}
+		System.out.println(primeiroValor);	
+		primeiroValor-=1;
+
+
+		System.out.println("par" + par);
+		System.out.println("impar" + impar);
+	}
+}
