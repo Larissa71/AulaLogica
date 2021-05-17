e3c0b35a59315cebb097676b990b426bc4b502ef
Larissa71 <larissakuffel@gmail.com>
2021-05-17 16:16:35 -0300

    refatoração de código

--- src/br/com/senai/Atividade3.java
+++ src/br/com/senai/Atividade3.java
@@ -7,27 +7,39 @@ public class Atividade3 {
 	
 		Scanner tec = new Scanner(System.in);
 		System.out.print("informe o primeiro valor: ");
+		int valor1 = tec.nextInt();
 		int primeiroValor = tec.nextInt();
 		System.out.print("informe o segundo valor: ");
 		int segundoValor = tec.nextInt();
+		int valor2 = tec.nextInt();
 
 		int par = 0;
 		int impar = 0;
 				
-		if(primeiroValor < segundoValor) {
-		while(primeiroValor <= segundoValor) {
+		if(valor1 < valor2) {
+			while (valor1 <= valor2)	{
+				if(valor1 % 2 == 0) {
+					par += 1;
+				}
+				else {
+					impar += 1;
+				}
+				
+				System.out.println(valor1);
+		valor1 += 1;
 			}
-		}if(par % 2 == 0) {
-			par++1 
+				
+	}
+		else if (valor1 > valor2) {
+		while (valor1 >= valor2) {
+			if(valor1 % 2 == 0 ) {
+				par += 1;
+			}
+			else {
+				impar += 1;
+			}
+			System.out.println(valor1); 
+			valor1 -= 1;		
 		}
-		}else{
-		while(primeiroValor > segundoValor) {
-		}
-		System.out.println(primeiroValor);	
-		primeiroValor-=1;
-
-
-		System.out.println("par" + par);
-		System.out.println("impar" + impar);
 	}
 }
